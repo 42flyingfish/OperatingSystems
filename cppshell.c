697c54cf9e2cc0f927782cc56dfb5384fd9fc15a
carmelo-og <51840169+carmelo-og@users.noreply.github.com>
2019-10-28 15:41:55 -0700

    Added comments to cppshell.c

--- cppshell.c
+++ cppshell.c
@@ -7,13 +7,12 @@
 #include "ls.h"
 #include "pwd.h"
 
-
-#define MAX_TOKEN_LENGTH 50
-#define MAX_TOKEN_COUNT 100
+#define MAX_CHAR_COUNT 100
 #define MAX_LINE_LENGTH 512
 #define SHELL_PROMPT "CPPShell: "
 
 void loop();
+void printPrompt();
 void runcommand(char* command, char** args);
 
 int main(int argc, char** argv) // args when calling shell, might not use at all -CE
@@ -25,15 +24,14 @@ int main(int argc, char** argv) // args when calling shell, might not use at all
 void loop()
 {
     char line[MAX_LINE_LENGTH];
-    printf("\033[1;35m");
-    printf("%s",SHELL_PROMPT); 
-    cd(""); 
+    printPrompt();
     
+    // get user input and parse tokens (keywords)
     while(fgets(line, MAX_LINE_LENGTH, stdin)) {
     	// Build the command and arguments, using execv conventions.
     	line[strlen(line)-1] = '\0'; // get rid of the new line
     	char* command = NULL;
-    	char* arguments[MAX_TOKEN_COUNT];
+    	char* arguments[MAX_CHAR_COUNT];
     	int argument_count = 0;
     	char* token = strtok(line, " ");
     	while(token)
@@ -62,21 +60,30 @@ void loop()
                 runcommand(command, arguments);
 	    }
         }
+        printPrompt();
+    }
+}
+
+void printPrompt()
+{
+    // magenta bold color prompt
         printf("\033[1;35m");
         printf("%s",SHELL_PROMPT);
+        // print directory inline
         cd(""); 
-    }
 }
 
 void runcommand(char* command, char** args)
 {
-  pid_t pid = fork();
-  if(pid)
-  { // parent
-	  waitpid(pid, NULL, 0);
-  }
-  else
-  { // child
-	  execvp(command, args);
-  }
+    // run executable in its own process using fork
+    pid_t pid = fork();
+    // fork will split to 2 instances then handled by the if, shell goes to true, command goes to else
+    if(pid)
+    {   // make shell wait for child process
+        waitpid(pid, NULL, 0);
+    }
+    else
+    {   // system call to run executable (child process)
+        execvp(command, args);
+    }
 }
