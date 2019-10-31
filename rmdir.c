96f31d103730356bf38fdd13ae1de195d53d7041
carmelo-og <51840169+carmelo-og@users.noreply.github.com>
2019-10-30 21:10:57 -0700

    Implemented basic non recursive rmdir (commented)

--- rmdir.c
+++ rmdir.c
@@ -22,7 +22,9 @@ void cpprmdir(char *args) {
 			perror("Error!");
 			return;
 		}
+		//Attempt remove of dir and return status, 0 means successful rem
 		int rem = remove(path);
+		//Show error if could not remove dir
 		if(rem != 0)
 			perror("Error!");
 	} else {
